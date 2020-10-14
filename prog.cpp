94ce72c4c265c55033b632bdc549fb8ff61f488d
fernandocunhapereira <fernandocunhapereira@gmail.com>
2020-10-14 15:27:56 -0300

    calculo de brancos e acertos por questao OK

--- prog.cpp
+++ prog.cpp
@@ -70,18 +70,35 @@ if(arquivo2.is_open()){
 	
 }else{
 	std::cout<<"Erro ao abrir arquivo";
-}
+}//=========================================================================
 
-//CALCULO DAS NOTAS DOS ALUNOS ===========================================
+//CALCULO DAS NOTAS DOS ALUNOS =============================================
 for(long int i=0;i<quantidade;i++){
 	for(int j=0;j<10;j++){
 		if(lista[i].respostas[j]==gabarito[j]){
-			lista[i].nota +=1;
+			lista[i].nota +=1; //incrementa nota do aluno
+		}
+		if(lista[i].respostas[j]!='A' && lista[i].respostas[j]!='B' && lista[i].respostas[j]!='C' && lista[i].respostas[j]!='D' && lista[i].respostas[j]!='E'){
+			questoes.brancos[j] +=1; //incrementa brancos na questao j
+		}
+		if(lista[i].respostas[j]==gabarito[j]){
+			questoes.acertos[j] +=1;
 		}
 	}std::cout<<lista[i].nome<<" nota "<<lista[i].nota<<"\n";
 }
-//===================================================================
+//==========================================================================
 
+//TOTAL DE BRANCOS E ACERTOS POR QUESTAO ===================================
+std::cout<<"brancos ";
+for(int i=0;i<10;i++){
+	std::cout<<questoes.brancos[i]<<" ";
+}
+std::cout<<std::endl;
+std::cout<<"acertos ";
+for(int i=0;i<10;i++){
+	std::cout<<questoes.acertos[i]<<" ";
+}
+//==========================================================================
 std::cout<<std::endl; 
 std::cout<<"fim teste"<<std::endl;
 return 0;
