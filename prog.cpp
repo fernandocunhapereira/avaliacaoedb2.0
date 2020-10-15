0f4a301ea766ecea90dcb677923132470af9b296
fernandocunhapereira <fernandocunhapereira@gmail.com>
2020-10-15 17:01:02 -0300

    resultados ordenados tambem pelo numero da questao

--- prog.cpp
+++ prog.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 
 struct Candidato{
 	std::string nome;
@@ -20,10 +19,9 @@ struct Questoes{
 int main(int argc, char const *argv[]){
 
 std::string linha;
-long int quantidade=0;
-long int contador=0;
+long int quantidade=0; //total de candidatos
 Candidato* lista;
-Questoes questoes;
+Questoes questoes; //variavel usada para guardar a quantidade de acertos e brancos por questao
 char gabarito[10]={'A','B','C','D','E','E','D','C','B','A'};
 std::string nomeArquivo=argv[1];
 std::string funcao=argv[2];
@@ -49,7 +47,7 @@ std::ifstream arquivo (nomeArquivo);
 if(arquivo.is_open()){
 	while(getline (arquivo,linha)){
 		//std::cout << linha << '\n';
-		quantidade +=1;
+		quantidade +=1; //quantidade é o total de candidatos no concurso
 	}
 	arquivo.close();
 }else{
@@ -59,20 +57,10 @@ if(arquivo.is_open()){
 
 lista=new Candidato[quantidade];
 
-//ATRIBUICAO DADOS DO ARQUIVO TXT NAS VARIAVEIS ============================
+//ATRIBUICAO DOS DADOS DO ARQUIVO TXT NAS VARIAVEIS ========================
 std::ifstream arquivo2 (nomeArquivo);
 
 if(arquivo2.is_open()){
-	// std::string nome;
-	// char resposta;
-	// while(!arquivo2.eof()){
-	// 	arquivo2>>nome;
-	// 	std::cout<<nome<<"\n";
-	// 	for(int i=0;i<10;i++){
-	// 		arquivo2>>resposta;
-	// 		std::cout<<resposta<<"\n";
-	// 	}
-	// }
 	for(long int i=0;i<quantidade;i++){
 		arquivo2>>lista[i].nome;
 		//std::cout<<lista[i].nome<<"\n";
@@ -81,20 +69,19 @@ if(arquivo2.is_open()){
 			//std::cout<<lista[i].respostas[j]<<"\n";
 		}
 	}
-	
 }else{
 	std::cout<<"Erro ao abrir arquivo\n";
 }
 //=========================================================================
 
-//CALCULO DAS NOTAS DOS ALUNOS; BRANCOS E ACERTOS POR QUESTAO ==============
+//CALCULO DAS NOTAS DOS ALUNOS; BRANCOS E ACERTOS POR QUESTAO =============
 for(long int i=0;i<quantidade;i++){
 	for(int j=0;j<10;j++){
 		if(lista[i].respostas[j]==gabarito[j]){
 			lista[i].nota +=1; //incrementa nota do aluno
 		}
 		if(lista[i].respostas[j]!='A' && lista[i].respostas[j]!='B' && lista[i].respostas[j]!='C' && lista[i].respostas[j]!='D' && lista[i].respostas[j]!='E'){
-			questoes.brancos[j] +=1; //incrementa brancos por questao 
+			questoes.brancos[j] +=1; //incrementa brancos por questao
 		}
 		if(lista[i].respostas[j]==gabarito[j]){
 			questoes.acertos[j] +=1; //incrementa acertos por questao
@@ -124,17 +111,17 @@ if(funcao=="best" || funcao=="worst"){
     		if(lista[min].nota>lista[j].nota){
     			min=j;
     		}	
-    	}										//SELECTION SORT
+    	}								//SELECTION SORT (ordem crescente)
     Candidato temp=lista[i];
     lista[i]=lista[min];
     lista[min]=temp;    	
 	}
-	if(funcao=="best"){ //imprime melhores
+	if(funcao=="best"){ //imprime melhores (do fim do vetor para inicio)
 		for(long int i=quantidade-1;i>=quantidade-ranking;i--){
 			std::cout<<lista[i].nome<<"\n";
 		}
 	}
-	if(funcao=="worst"){ //imprime piores
+	if(funcao=="worst"){ //imprime piores (do inicio do vetor para fim)
 		for(long int i=0;i<ranking;i++){
 			std::cout<<lista[i].nome<<"\n";
 		}	
@@ -150,17 +137,30 @@ if(funcao=="blank-questions"){
     		if(questoes.brancos[min]>questoes.brancos[j]){
     			min=j;
     		}	
-    	}										//SELECTION SORT
+    	}								//SELECTION SORT (ordem crescente)
     long int temp=questoes.brancos[i];
     questoes.brancos[i]=questoes.brancos[min];
     questoes.brancos[min]=temp;     	
 	
-	int temp2=questoes.numeroQuestoes[i];
+	int temp2=questoes.numeroQuestoes[i]; //vetor com o numero das questoes é ordenado conforme vetor de quantidade de brancos
     questoes.numeroQuestoes[i]=questoes.numeroQuestoes[min];
     questoes.numeroQuestoes[min]=temp2;     	
-	}
-	for(int i=9;i>9-ranking;i--){
-		std::cout<<questoes.numeroQuestoes[i]<<"\n";
+	}// ok daqui pra cima ===========
+
+	for(int i=0;i<9;i++){ //===== ordenando resultado anterior pelo numero da questao (as que tem valores iguais)
+		int min=i;
+		for(int j=i+1;j<10;j++){
+			if(questoes.brancos[min]==questoes.brancos[j] && questoes.numeroQuestoes[min]<questoes.numeroQuestoes[j]){
+				min=j;
+			}
+		}
+	int temp3=questoes.numeroQuestoes[i]; 
+    questoes.numeroQuestoes[i]=questoes.numeroQuestoes[min];
+    questoes.numeroQuestoes[min]=temp3; 	
+	} //======
+
+	for(int i=9;i>9-ranking;i--){ //impressao OK de brancos
+ 		std::cout<<questoes.numeroQuestoes[i]<<"\n";
 	}
 }
 //==========================================================================
@@ -173,21 +173,34 @@ if(funcao=="best-questions" || funcao=="worst-questions"){
     		if(questoes.acertos[min]>questoes.acertos[j]){
     			min=j;
     		}	
-    	}										//SELECTION SORT
+    	}								//SELECTION SORT (ordem crescente)
     long int temp=questoes.acertos[i];
     questoes.acertos[i]=questoes.acertos[min];
     questoes.acertos[min]=temp;     	
 	
-	int temp2=questoes.numeroQuestoes[i];
+	int temp2=questoes.numeroQuestoes[i]; //vetor com o numero das questoes é ordenado conforme vetor com quantidade de acertos
     questoes.numeroQuestoes[i]=questoes.numeroQuestoes[min];
     questoes.numeroQuestoes[min]=temp2;     	
-	}
-	if(funcao=="best-questions"){ //imprime questoes com mais acertos
+	}// ok daqui pra cima =================================
+
+	for(int i=0;i<9;i++){ //===== ordenando resultado anterior pelo numero da questao (as que tem valores iguais)
+		int min=i;
+		for(int j=i+1;j<10;j++){
+			if(questoes.acertos[min]==questoes.acertos[j] && questoes.numeroQuestoes[min]<questoes.numeroQuestoes[j]){
+				min=j;
+			}
+		}
+	int temp4=questoes.numeroQuestoes[i]; 
+    questoes.numeroQuestoes[i]=questoes.numeroQuestoes[min];
+    questoes.numeroQuestoes[min]=temp4; 	
+	} //======
+
+	if(funcao=="best-questions"){ //imprime questoes com mais acertos (do fim do vetor para inicio)
 		for(int i=9;i>9-ranking;i--){
 			std::cout<<questoes.numeroQuestoes[i]<<"\n";
 		}
 	}
-	if(funcao=="worst-questions"){ //imprime questoes com menos acertos
+	if(funcao=="worst-questions"){ //imprime questoes com menos acertos (do inicio do vetor para fim)
 		for(int i=0;i<ranking;i++){
 			std::cout<<questoes.numeroQuestoes[i]<<"\n";
 		}
@@ -200,7 +213,6 @@ if(funcao=="best-questions" || funcao=="worst-questions"){
 
 //IMPRESSAO DE ALGUMAS VARIAVEIS ===========================================
 std::cout<<std::endl;
-std::cout<<"contador "<<contador<<std::endl;
 std::cout<<"quantidade de candidatos "<<quantidade<<std::endl;
 std::cout<<"nome do arquivo "<<nomeArquivo<<std::endl;;
 std::cout<<"nome da funcao "<<funcao<<std::endl;
